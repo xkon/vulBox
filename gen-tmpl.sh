c4b977e05fba393bdd4051ec52e9592b44961aba
xkon <xalvint@163.com>
2016-08-19 14:26:26 +0800

    修正镜像名字只能小写的问题

--- gen-tmpl.sh
+++ gen-tmpl.sh
@@ -4,4 +4,8 @@ CVENUM=$1
 mkdir $CVENUM
 cp tmpl.md $CVENUM/readme.md
 sed -i "s/CVENUM/$CVENUM/g" $CVENUM/readme.md
+
+# images name can only consist of lowercase (a-z) characters
+cvenum=`echo $CVENUM | tr 'A-Z' 'a-z'`
+sed -i "s#xk0n/$CVENUM#xk0n/$cvenum#g" $CVENUM/readme.md
 echo "[*] Created $CVENUM directory "
