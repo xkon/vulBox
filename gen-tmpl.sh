43ec2ed4a6b006c9e1a4ec23702fdf4c2864f584
xkon <xalvint@gmail.com>
2017-07-05 19:57:14 +0800

    add CVE-2016-6321 GNU tar 解压路径穿越漏洞

--- gen-tmpl.sh
+++ gen-tmpl.sh
@@ -8,4 +8,5 @@ sed -i "s/CVENUM/$CVENUM/g" $CVENUM/readme.md
 # images name can only consist of lowercase (a-z) characters
 cvenum=`echo $CVENUM | tr 'A-Z' 'a-z'`
 sed -i "s#xk0n/$CVENUM#xk0n/$cvenum#g" $CVENUM/readme.md
+cd $CVENUM
 echo "[*] Created $CVENUM directory "
